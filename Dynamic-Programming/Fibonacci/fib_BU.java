e8b1225a9ff8a8e2d5b4e339ba3016962bc67370
Prakash Natarajan <prakash.natarajan@outlook.com>
2014-06-15 10:23:02 +0530

    Update fib_BU.java

--- Dynamic-Programming/Fibonacci/fib_BU.java
+++ Dynamic-Programming/Fibonacci/fib_BU.java
@@ -6,7 +6,7 @@ class fib_BU {
 	int[] lookup = new int[41]; // 40 is the fib number we are abt to calculate
 
 	public static void main(String[] args) {
-		fib_DP program = new fib_DP();
+		fib_BU program = new fib_BU();
 		//program.intialize();
 		System.out.print("fib of 40 :" + program.fib(40));
 	}
