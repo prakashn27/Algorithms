ab0fe3ec43c299b8495e54af0474460fde884769
Prakash Natarajan <nprakashece@gmail.com>
2016-04-26 18:34:16 -0400

    added SAP of PA1

--- University-Assignments/Princeton/algo2/src/algo2/SAP.java
+++ University-Assignments/Princeton/algo2/src/algo2/SAP.java
@@ -3,6 +3,11 @@
  */
 package algo2;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
@@ -19,25 +24,88 @@ public class SAP {
 	   public SAP(Digraph G) {
 		   paths = G;
 	   }
-
+	   Map<Integer, Integer> getAncestor(int v) {
+		  Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		  Queue<Integer> q = new LinkedList<Integer>();
+		  q.add(v);
+		  map.put(v, 0);
+		  while(!q.isEmpty()) {
+			Integer head = q.remove();
+			Integer dist = map.get(head);
+			for(Integer w: paths.adj(head)) {
+				if(!map.containsKey(w) || (map.get(w) > dist + 1)) {
+					map.put(w, dist + 1);
+				} 
+				q.add(w);
+			}
+		  }
+		  return map;
+		   
+	   }
 	   // length of shortest ancestral path between v and w; -1 if no such path
 	   public int length(int v, int w) {
-		   return 0;
+		   Map<Integer, Integer> v2dist = getAncestor(v);
+		   Map<Integer, Integer> w2dist = getAncestor(w);
+		   int dist = -1;
+		   for(Integer vkey : v2dist.keySet()) {
+			   if(w2dist.containsKey(vkey)) {
+				   int currentDist = v2dist.get(vkey) + w2dist.get(vkey);
+				   if(dist < 0 || currentDist < dist) {
+					   dist = currentDist; 
+				   }
+			   }
+		   }
+		   return dist;
 	   }
 
 	   // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
 	   public int ancestor(int v, int w) {
-		   return 0;
+		   Map<Integer, Integer> v2dist = getAncestor(v);
+		   Map<Integer, Integer> w2dist = getAncestor(w);
+		   int dist = -1;
+		   int anc = -1;
+		   for(Integer vkey : v2dist.keySet()) {
+			   if(w2dist.containsKey(vkey)) {
+				   int currentDist = v2dist.get(vkey) + w2dist.get(vkey);
+				   if(dist < 0 || currentDist < dist) {
+					   dist = currentDist; 
+					   anc = vkey;
+				   }
+			   }
+		   }
+		   return anc;
 	   }
 
 	   // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
 	   public int length(Iterable<Integer> v, Iterable<Integer> w) {
-		   return 0;
+		   int dist = -1;
+		   for(Integer vv : v) {
+			   for(Integer ww: w) {
+				   int currentDist = length(vv,ww);
+				   if(dist < 0 || currentDist < dist) {
+					   dist = currentDist;
+				   }
+			   }
+		   }
+		   
+		   return dist;
 	   }
 
 	   // a common ancestor that participates in shortest ancestral path; -1 if no such path
 	   public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-		   return 0;
+		   int dist = -1;
+		   int anc = -1;
+		   for(Integer vv : v) {
+			   for(Integer ww: w) {
+				   int currentDist = length(vv,ww);
+				   if(dist < 0 || currentDist < dist) {
+					   dist = currentDist;
+					   anc = ancestor(vv, ww);
+				   }
+			   }
+		   }
+		   
+		   return anc;
 	   }
 
 	   // do unit testing of this class
