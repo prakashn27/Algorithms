6b04227a935907bf236ff282691f8e9747c7b8a0
Prakash Natarajan <nprakashece@gmail.com>
2016-04-25 21:03:01 -0400

    added first draft of Week 1 :construction_worker:

---
+++ University-Assignments/Princeton/algo2/src/algo2/WordNet.java
@@ -0,0 +1,101 @@
+package algo2;
+import java.lang.Iterable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.StdIn;
+
+public class WordNet {
+
+   HashMap<Integer, Synset> id2synset;
+   Digraph G;
+   HashMap<String, Bag<Integer>> noun2id;
+   
+   // constructor takes the name of the two input files
+   public WordNet(String synsets, String hypernyms){
+	   noun2id = new HashMap<String, Bag<Integer>>();
+	   id2synset = new HashMap<Integer, Synset>();
+		 //Read data from synsets input file 
+		In in = new In(synsets);
+		String[] line;
+		String[] synonyms;
+		Synset synset;
+		int id;
+		while(!in.isEmpty()) {
+			synset = new Synset();
+			line = in.readLine().split(",");
+			synset.synonyms = line[1];
+			synset.set_nouns = new HashSet<String>();
+			id = Integer.parseInt(line[0]);
+			synonyms = line[1].split(" ");
+			for(String s : synonyms) {
+				synset.set_nouns.add(s);
+				if(!noun2id.containsKey(s)) {
+					Bag<Integer> bag = new Bag<Integer>();
+					bag.add(id);
+					noun2id.put(s, bag);
+				} else {
+					noun2id.get(s).add(id);
+				}
+			}
+			synset.definition = line[2];
+			id2synset.put(id, synset);
+		}
+		
+		G = new Digraph(id2synset.size());
+		// Read data from hypernyms input file
+		in = new In(hypernyms);
+		int v, w;
+		while(!in.isEmpty()) {
+			line =  in.readLine().split(",");
+			w = Integer.parseInt(line[0]);
+			for(int i = 1; i < line.length; i++) {
+				v = Integer.parseInt(line[i]);
+				G.addEdge(v, w);
+			}
+		}
+   }
+
+   // returns all WordNet nouns
+   public Iterable<String> nouns() {
+	   return noun2id.keySet();
+   }
+
+   // is the word a WordNet noun?
+   public boolean isNoun(String word) {
+	   return noun2id.containsKey(word);
+   }
+
+   // distance between nounA and nounB (defined below)
+   public int distance(String nounA, String nounB) {
+	   // run BFS to the distance
+	   Bag<Integer> id1_list = noun2id.get(nounA);
+	   Bag<Integer> id2_list = noun2id.get(nounB);
+	   
+	   return 0;
+   }
+
+   // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+   // in a shortest ancestral path (defined below)
+   public String sap(String nounA, String nounB) {
+	   return new String();
+   }
+
+   // do unit testing of this class
+   public static void main(String[] args) {
+	   WordNet wn = new WordNet("reference_files/wordnet/synsets.txt", "reference_files/wordnet/hypernyms.txt");
+	   System.out.println("Hello World!");
+   }
+
+}
+
+class Synset {
+	public String synonyms;
+	public Set<String> set_nouns;
+	public String definition;
+}
