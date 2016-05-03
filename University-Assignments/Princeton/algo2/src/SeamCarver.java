dca6558f506c75581bbd0ba02a694af672d77879
Prakash Natarajan <nprakashece@gmail.com>
2016-05-02 22:29:06 -0400

    added boiler plate of Seamcarver

---
+++ University-Assignments/Princeton/algo2/src/SeamCarver.java
@@ -0,0 +1,58 @@
+import edu.princeton.cs.algs4.Picture;
+
+/**
+ * @author prakashn
+ *
+ */
+public class SeamCarver {
+	public SeamCarver(Picture picture) // create a seam carver object based on the given picture
+	{
+		
+	}
+	public Picture picture()  // current picture
+	{
+		return null;
+		
+	}
+	public  int width() // width of current picture
+	{
+		return 0;
+		
+	}
+	public  int height()// height of current picture
+	{
+		return 0;
+		
+	}
+	public  double energy(int x, int y)// energy of pixel at column x and row y
+	{
+		return y;
+		
+	}
+	public int[] findHorizontalSeam()// sequence of indices for horizontal seam
+	{
+		return null;
+		
+	}
+	public int[] findVerticalSeam()  // sequence of indices for vertical seam
+	{
+		return null;
+		
+	}
+	public void removeHorizontalSeam(int[] seam)// remove horizontal seam from current picture
+	{
+		
+	}
+	public void removeVerticalSeam(int[] seam)  // remove vertical seam from current picture
+	{
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
